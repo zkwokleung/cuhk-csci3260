243cab07d11f0c8441b0e72c42ce890f749756a2
Andrew SZETO <zkwokleung@gmail.com>
2022-09-26 19:00:11 +0800

    Update Fragment shader

--- Assignment1/FragmentShaderCode.glsl
+++ Assignment1/FragmentShaderCode.glsl
@@ -3,16 +3,7 @@
 layout(location = 0) out vec4 FragColor;
 in vec3 vertColor;
 
-uniform vec4 u_VertColor;
-
 void main()
 {
-	if(u_VertColor != 0)
-	{
-		FragColor = u_VertColor;
-	}
-	else
-	{
-		FragColor = vec4(vertColor, 1.0f);
-	}
+	FragColor = vec4(vertColor, 1.0f);
 }
